eab539600448308b92731d7ff261427ae9c61aaf
yscope <thienyglobe@gmail.com>
2022-09-19 00:02:05 +0700

    fix ecode long sequence

--- model/encoder/graph_input.py
+++ model/encoder/graph_input.py
@@ -104,7 +104,7 @@ class GraphInputLayerPLM(nn.Module):
         question_output = self._bert_encode(question_ids, batch)
         # number of table x max table name len x hidden size
         table_output = self._bert_encode(tabs, batch)
-        tabs = tabs.masked_select(table_mask.unsqueeze(-1))
+        tabs = table_output.masked_select(table_mask.unsqueeze(-1))
         tabs = tabs.reshape((len(sample.table_id), self.config.hidden_size))
         col_output, last_sep = self._bert_en_code(cols, batch, True, sample)
         col_output = col_output.masked_select(col_mask.unsqueeze(-1))
